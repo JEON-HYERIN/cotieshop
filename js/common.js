ac55b27d21702aa8376cbc4d4617a9e9ea612910
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-01 23:37:05 +0900

    경로변경

--- js/common.js
+++ js/common.js
@@ -159,7 +159,7 @@ function changeImg () {
   var imgUrl = ['punch1.svg', 'punch2.svg', 'punch3.svg', 'punch4.svg'];
   var chosenImgUrl = imgUrl[Math.floor(Math.random() * imgUrl.length)];
   
-  imgEl.setAttribute('src', `/img/${chosenImgUrl}`);
+  imgEl.setAttribute('src', `D:/DNA(D)/GitHub/cotieshop/cotieshop/img/${chosenImgUrl}`);
 }
 
 // SCROLL MAGIC
