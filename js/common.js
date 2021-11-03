6d7e056ec5518277c4835fb9309b1fe3c081d077
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-03 21:35:51 +0900

    수정

--- js/common.js
+++ js/common.js
@@ -166,7 +166,6 @@ function changeImg () {
   var imgUrl = ['punch1.svg', 'punch2.svg', 'punch3.svg', 'punch4.svg'];
   var chosenImgUrl = imgUrl[Math.floor(Math.random() * imgUrl.length)];
   
-  // imgEl.setAttribute('src', `D:/DNA(D)/GitHub/cotieshop/cotieshop/img/${chosenImgUrl}`);
   imgEl.setAttribute('src', `https://jeon-hyerin.github.io/cotieshop/img/${chosenImgUrl}`);
 }
 
